4d0c1fa1cc3d2edd749f955c0c1b750e5efbd53c
fevi29 <fevenbayu5@gmail.com>
2024-01-11 10:44:35 +0000

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,8 @@
-quare_matrix_simple(matrix=[]):
-    return [[y ** 2 for y in x] for x in matrix]
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
